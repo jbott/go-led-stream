d0637644643ba29e9737dc06761e870f44cd4d43
John Ott <john.ott@johnott.us>
2016-08-21 14:08:54 -0400

    Restart led service on build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-GOOS=linux GOARCH=arm GOARM=7 go build && rsync -av * pi:~/go-led-stream/
+GOOS=linux GOARCH=arm GOARM=7 go build && rsync -av * pi:~/go-led-stream/ && ssh pi 'sudo systemctl restart led-stream'
